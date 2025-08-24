9627cbee58d3cffdfc0b6d8ff79b785d24fc21a5
lululthfh <punyalulu05@gmail.com>
2025-08-25 02:12:29 +0700

    feat: add get method for ktp-data

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const db = require("./db/db.js");
+
+app.use(express.json());
+app.get("/ktp-data", (req, res) => {  
+  db.query('SELECT * FROM ktp', (err, results) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.json(results);
+    });
+});
+
+db.query("SELECT * from todos", (err, todos) => {
+    if (err) return res.status(500).send("Internal Server Error");
+    res.render("todo", {
+      todos: todos,
+      layout: "layouts/main-layout",
+    });
+  });
