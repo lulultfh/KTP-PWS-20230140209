29e2961cb224e39d2b5b9bad04a1fcf29b049f08
lululthfh <punyalulu05@gmail.com>
2025-08-25 02:26:52 +0700

    feat: add post method

--- index.js
+++ index.js
@@ -14,7 +14,38 @@ app.get("/ktp-data", (req, res) => {
 app.get('/:nik', (req, res) => {
     db.query('SELECT * FROM ktp WHERE nik = ?', [req.params.id], (err, results) => {
         if (err) return res.status(500).send('Internal Server Error');
-        if (results.length === 0) return res.status(404).send('Tugas tidak ditemukan');
+        if (results.length === 0) return res.status(404).send('KTP tidak ditemukan');
         res.json(results[0]);
+    });
+});
+
+app.post('/ktp-data', (req, res) => {
+    const { nik, nama, tempat_lahir, tanggal_lahir, jns_kelamin, alamat, agama, status_kawin, pekerjaan, kewarganegaraan } = req.body;
+    if (!nik || !nama || !tempat_lahir || !tanggal_lahir || !jns_kelamin || !alamat || !agama|| !status_kawin || !pekerjaan || !kewarganegaraan) {
+        return res.status(400).send('Semua kolom wajib diisi!');
+    }
+    const query = 'INSERT INTO ktp (nik, nama, tempat_lahir, tanggal_lahir, jns_kelamin, alamat, agama, status_kawin, pekerjaan, kewarganegaraan) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [nik.trim(), jns_kelamin.trim(), agama.trim()];
+
+    db.query(query, values, (err) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+
+        const newKTP = {
+            nik: nik.trim(),
+            nama: nama,
+            tempat_lahir: tempat_lahir,
+            tanggal_lahir: tanggal_lahir,
+            jns_kelamin: jns_kelamin.trim(),
+            alamat: alamat,
+            agama: agama.trim(),
+            status_kawin: status_kawin,
+            pekerjaan: pekerjaan,
+            kewarganegaraan: kewarganegaraan
+        };
+
+        res.status(201).json(newKTP);
     });
 });
