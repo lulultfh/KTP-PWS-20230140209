e59bf9aff4f2c6430f193e6227a29e564135676c
lululthfh <punyalulu05@gmail.com>
2025-09-06 23:03:14 +0700

    feat: update path

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const ktpRoutes = require("./routes/ktp-db");
+const ktpRoutes = require("./routes/ktp-db.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// const PORT = 3000;
 
 app.use(cors());
 app.use(express.static('public'));
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use('/api/ktp', ktpRoutes);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(PORT, () => {
